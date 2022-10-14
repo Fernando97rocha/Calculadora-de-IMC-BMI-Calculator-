282190135b7522988e1d22b7268f73aae09dd366
Fernando Rodrigues <nandocf20@gmail.com>
2022-10-14 08:59:09 -0300

    atualização código JS

--- Assets/app.js
+++ Assets/app.js
@@ -1,21 +1,24 @@
 const mostraCalculo = () => {
     
-        const weight = document.querySelector("#weight").value
-        const height = document.querySelector("#height").value
-        const mainContainer = document.querySelector('#div-main')
+    const weight = document.querySelector("#weight").value
+    const height = document.querySelector("#height").value
+    const mainContainer = document.querySelector('#div-main')
+    const messageError = document.querySelector('.messageError')
     
-
+    
     if(typeof weight === undefined || typeof height === undefined) {
         throw Error("Os parâmetros devem ser passados")
     } else if (!weight || !height){
         throw Error("Os parâmetros devem ser passados")
-    } else if (weight === 0 || height === 0) {
-        mainContainer.innerHTML = 'sss'
+    } else if (weight < 10 || height < 10 ) {
+        messageError.innerHTML = `<p>asdfasfkfk</p>`
     } else;
+    
+    const valuesCalculate = Math.trunc((weight / height ** 2) * 10000)
 
-    let valor = Math.trunc((weight / height ** 2) * 10000)
-    let frase
-
+    let valor = valuesCalculate
+    let frase = ''
+    
     if(valor < 18.5) {
         frase = "Você está abaixo do peso!"
     } else if (valor <= 24.9) {
@@ -29,11 +32,12 @@ const mostraCalculo = () => {
     } else if (valor > 40) {
         frase = "Obesidade grau III"
     } else;
-
-    mainContainer.innerHTML = `<section class="resultado">Seu IMC é ${valor} <br> ${frase}</section>
+    
+    const resultMessage = `<section class="resultado">Seu IMC é ${valor} <br> ${frase}</section>
         <input type="button" value="Voltar" onclick="history.go(0)"></input>`
     
-
+    mainContainer.innerHTML = resultMessage
+    
     const apagaCampos = () => {
         document.querySelector("#weight").value = ''
         document.querySelector("#height").value = ''
